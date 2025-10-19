79847c22301d7a3a345eaff092a0d7a51769950e
masukomi <masukomi@masukomi.org>
2025-10-19 10:47:04 -0400

    updated mozilla sidebar script

---
+++ mozilla_docs_sidebar_removal.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Mozilla Docs Sidebar Removal
+// @namespace    http://masukomi.org/
+// @version      2024-06-18
+// @description  remove useless sidebary so that i can have space to read content.
+// @author       You
+// @match        https://developer.mozilla.org/en-US/docs/Web/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// @grant        GM_addStyle
+// ==/UserScript==
+
+GM_addStyle(`
+#content {
+  min-width: fit-content;
+}
+.main-wrapper {
+  display: flex;
+}
+`);
